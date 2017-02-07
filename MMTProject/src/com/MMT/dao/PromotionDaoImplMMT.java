a652ac4446e687fa20071c5fcd37ef57ac698815
shreyoshi <shreyoshimahato@gmail.com>
2017-02-07 14:25:56 +0530

    pro

--- MMTProject/src/com/MMT/dao/PromotionDaoImplMMT.java
+++ MMTProject/src/com/MMT/dao/PromotionDaoImplMMT.java
@@ -1,6 +1,7 @@
 package com.MMT.dao;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -16,6 +17,7 @@ public class PromotionDaoImplMMT implements PromotionDaoMMT {
 	@Override
 	public int insertPromotion(Promotion p)throws SQLException {
 		con=DbConnection.dbConnection();
+		
 		String promotionId=p.getPromotionId();
 		String promotionName=p.getPromotionName();
 		float promotionDiscount=p.getPromotionDiscount();
@@ -49,28 +51,66 @@ public class PromotionDaoImplMMT implements PromotionDaoMMT {
 	@Override
 	public int deletePromotion(String promotionId)throws SQLException {
 		con=DbConnection.dbConnection();
-		con=null;
+		Statement stmt=con.createStatement();
+		int rows=stmt.executeUpdate("delete from PROMOTION where PROMOTIONID ="+promotionId);
+		if(rows>0)
+		{
+			con.close();
+			return rows;
+		}
+		else 
+		{	con.close();
 		return 0;
+		}
 	}
-
 	@Override
-	public int updatePromotion(String promotionId, Promotion newPromotion) throws SQLException{
+	public int updatePromotion(String pId, Promotion newp) throws SQLException{
 		con=DbConnection.dbConnection();
-		con=null;
+		String promotionId=newp.getPromotionId();
+		String promotionName=newp.getPromotionName();
+		float promotionDiscount=newp.getPromotionDiscount();
+		Date promotionExp=newp.getPromotionExpiryDate();
+		float promotionReqAmt=newp.getPromotionMinRequiredAmount();
+		String promotionType=newp.getPromotionType();
+		Statement stmt=con.createStatement();
+		int rows=stmt.executeUpdate("update PROMOTION set PROMO"
+				+ "TIONID= '"+promotionId+"' ,PROMOTIONNAME ='"+promotionName+"',PROMOTIONDI"
+						+ "SCOUNT='"+promotionDiscount+"' ,PROMOTIONEXPIRYDATE='"+promotionExp+"',PROMOTIONMINREQ"
+								+ "UIREDAMOUNT='"+promotionReqAmt+"',PROMOTIONTYPE ='"+promotionType+"' where PROMOTIONID="+promotionId);
+		if(rows>0)
+		{
+			con.close();
+			return rows;
+		}
+		else 
+		{	con.close();
 		return 0;
+		}
 	}
 
 	@Override
 	public ArrayList<Promotion> displayPromotion()throws SQLException {
+		Promotion pro=new Promotion();
 		con=DbConnection.dbConnection();
-		con=null;
+		ArrayList<Promotion> proList=new ArrayList<Promotion>();
+		Statement stmt=con.createStatement();
+		ResultSet rs=stmt.executeQuery("select * from Promotion ");
+		while(rs.next()){
+			pro.setPromotionId(rs.getString(1));
+			pro.setPromotionName(rs.getString(2));
+			pro.setPromotionDiscount(rs.getFloat(3));
+			pro.setPromotionExpiryDate(rs.getDate(4));
+			pro.setPromotionMinRequiredAmount(rs.getFloat(5));
+			pro.setPromotionType(rs.getString(6));
+			
+		}
 		return null;
 	}
 
 	@Override
 	public Promotion searchPromotion(String promotionId) throws SQLException{
 		con=DbConnection.dbConnection();
-		con=null;
+		
 		return null;
 	}
 
