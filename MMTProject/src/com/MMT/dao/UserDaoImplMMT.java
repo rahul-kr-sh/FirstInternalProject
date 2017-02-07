7db97220855c7f9e2057e460ba4daeca83c62292
Rupank <rupankbansal@gmail.com>
2017-02-07 15:20:58 +0530

    asdasd

--- MMTProject/src/com/MMT/dao/UserDaoImplMMT.java
+++ MMTProject/src/com/MMT/dao/UserDaoImplMMT.java
@@ -13,13 +13,12 @@ public class UserDaoImplMMT implements UserDaoMMT {
 	public int insert(User user) throws SQLException {
 		int row;
 		Connection con=DbConnection.dbConnection();
-		PreparedStatement pst=con.prepareStatement("insert into movie_list values(?,?,?,?,?,?)");
+		PreparedStatement pst=con.prepareStatement("insert into movie_list values(?,?,?,?,?)");
 		pst.setString(1, user.getUserId());
 		pst.setString(2,user.getUserName());
 		pst.setLong(3,user.getUserPhoneNo());
 		pst.setString(4,user.getUserEmailId());
 		pst.setString(5,user.getUserAddress());
-		pst.setString(6,user.getUserWalletId());
 		
 		
 		row=pst.executeUpdate();
