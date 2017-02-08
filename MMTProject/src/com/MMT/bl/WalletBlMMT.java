7302981694fcbc9f240b81ef18eb99a9e81c50e2
Rupank <rupankbansal@gmail.com>
2017-02-08 13:14:58 +0530

    Wallet BL Logic Done

---
+++ MMTProject/src/com/MMT/bl/WalletBlMMT.java
@@ -0,0 +1,47 @@
+package com.MMT.bl;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.MMT.bean.Wallet;
+import com.MMT.dao.WalletDaoImplMMT;
+import com.MMT.dao.WalletDaoMMT;
+
+public class WalletBlMMT {
+	private WalletDaoMMT W = new WalletDaoImplMMT();
+
+	public float walletBalance(String userId) throws SQLException {
+		Wallet w;
+
+		w = W.displayWallet(userId);
+		return (float) w.getWalletBalance();
+	}
+
+	public ArrayList<Wallet> displayAll() throws SQLException {
+		return W.displayWalletAll();
+	}
+
+	private boolean addWalletMoney(String userId,Double value) throws SQLException {
+		Wallet w;
+		w=W.displayWallet(userId);
+		w.setWalletBalance(w.getWalletBalance()+value);
+		W.updateWallet(userId, w);
+		return true;
+	}
+	
+	private boolean subtractWalletMoney(String userId,Double value) throws SQLException {
+		Wallet w;
+		w=W.displayWallet(userId);
+		double temp=w.getWalletBalance()-value;
+		if(temp<0){
+			//Exception
+			return false;
+		}
+		else{
+			w.setWalletBalance(temp);
+			W.updateWallet(userId, w);
+			return true;
+		}
+		
+	}
+}
