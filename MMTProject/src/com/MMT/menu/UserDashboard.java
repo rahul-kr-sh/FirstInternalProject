a56dfec59b07d396577af0271e570ebafd9c96ab
Rupank <rupankbansal@gmail.com>
2017-02-08 15:20:56 +0530

    User Dashboard Started

--- MMTProject/src/com/MMT/menu/UserDashboard.java
+++ MMTProject/src/com/MMT/menu/UserDashboard.java
@@ -1,11 +1,14 @@
 package com.MMT.menu;
 
+import java.util.Scanner;
+
 import com.MMT.bean.User;
 
 public class UserDashboard {
+	Scanner sc=new Scanner(System.in);
 	public void showDashboard(User user){
-		
-		System.out.println("Welcome "+ user.getUserName());
+		System.out.println("-------------User Dashboard-----------");
+		System.out.println("Welcome "+ user.getUserName()+"!!");
 		System.out.println("1. Flight");
 		System.out.println("2. Hotel");
 		System.out.println("3. View Past Flight Bookings");
@@ -13,5 +16,24 @@ public class UserDashboard {
 		System.out.println("5. Add Money to Wallet");
 		System.out.println("6. Logout");
 		
+		System.out.println("Enter a choice: ");
+		int input=sc.nextInt();
+		
+		switch(input){
+		case 1:
+			
+			return;
+			
+		case 2:
+			return;
+		case 3:
+			return;
+		case 4:
+			return;
+		case 5:
+			return;
+		case 6:
+			return;
+		}
 	}
 }
