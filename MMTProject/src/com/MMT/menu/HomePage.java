edfd9db6ea28fafd1e78d7510e4ce2b4e1549731
Rupank <rupankbansal@gmail.com>
2017-02-08 14:55:57 +0530

    Menu Started

---
+++ MMTProject/src/com/MMT/menu/HomePage.java
@@ -0,0 +1,40 @@
+package com.MMT.menu;
+
+import java.util.Scanner;
+
+public class HomePage {
+	public void HomePageMenu(){
+		System.out.println("-------------HomePage--------------");
+		
+		System.out.println("1.Login");
+		System.out.println("2. Signup");
+		System.out.println("3. Flight");
+		System.out.println("4. Hotel");
+		System.out.println("5. Exit");
+		System.out.println("Pick a option from Menu");
+		Scanner sc=new Scanner(System.in);
+		int input=sc.nextInt();
+		
+		switch(input){
+		case 1:
+			 
+			return;
+		case 2:
+			return;
+		
+		case 3:
+			return;
+		
+		case 4:
+			return;
+			
+		case 5:
+			return;
+		
+		default:
+			System.out.println("Invalid Input");
+			HomePageMenu();
+			break;
+		}
+	}
+}
---
+++ MMTProject/src/com/MMT/menu/LoginMenu.java
@@ -0,0 +1,55 @@
+package com.MMT.menu;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+import com.MMT.bean.Admin;
+import com.MMT.bean.User;
+import com.MMT.bl.AdminBlMMT;
+import com.MMT.bl.UserBlMMT;
+
+public class LoginMenu {
+	AdminBlMMT Ad=new AdminBlMMT();
+	UserBlMMT Us=new UserBlMMT();
+	Scanner sc=new Scanner(System.in);
+	public void LoginPage(){
+		System.out.println("Enter User Name:");
+		String name=sc.next();
+		System.out.println("Enter Password: ");
+		String pass=sc.next();
+		
+		if(name==null){
+			System.out.println("THE USERNAME FIELD IS BLANK!!!");
+		}
+		else if(pass==null){
+			System.out.println("THE PASSWORD FIELD IS BLANK!!!");
+		}
+		else{
+			
+			try {
+				if(Ad.checkAdminLogin(name, pass)!=null)
+					{
+					
+					System.out.println(" Successful Admin Login!!");
+					Admin admin=Ad.checkAdminLogin(name, pass);
+				//	adminDisplay();
+					
+					}
+				else if(Us.checkLogin(name, pass)!=null)
+				{
+					System.out.println(" Successful User Login!!");
+					User user = Us.checkLogin(name, pass);
+					//User Display
+				}
+				else{
+					System.out.println("Invalid Credentials");
+					return;
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return;
+	}
+}
