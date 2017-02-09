bdf208bb5d59deea993fb596a7f8b6acce59b816
priyanka <15priyankat@gmail.com>
2017-02-09 10:56:31 +0530

    helperHotel and rooms

---
+++ MMTProject/src/com/MMT/helper/HotelHelperMMT.java
@@ -0,0 +1,33 @@
+package com.MMT.helper;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.MMT.bean.Hotel;
+import com.MMT.bean.HotelRoom;
+public class HotelHelperMMT {
+	HotelRoomHelperMMT hrHelp=new HotelRoomHelperMMT();
+	ArrayList<HotelRoom> arrayListHotelRoom=new ArrayList<HotelRoom>();
+	Scanner sc=new Scanner(System.in); 
+	Hotel hotel=new Hotel();
+	public void input(){
+		System.out.println("Enter Hotel Id :");
+		hotel.setHotelId(sc.next());
+		System.out.println("Enter Hotel Name :");
+		hotel.setHotelName(sc.next());
+		System.out.println("Enter Hotel Location :");
+		hotel.setHotelLocation(sc.next());
+		System.out.println("Enter Information :");
+		hotel.setHotelInfo(sc.next());
+		System.out.println("");
+		 hrHelp.input();
+		 arrayListHotelRoom=hrHelp.getArrayListHotelRoom();
+		hotel.setHotelRoom(arrayListHotelRoom);
+	}
+	public Hotel getHotel() {
+		return hotel;
+	}
+	public void setHotel(Hotel hotel) {
+		this.hotel = hotel;
+	}
+	
+}
---
+++ MMTProject/src/com/MMT/helper/HotelRoomHelperMMT.java
@@ -0,0 +1,43 @@
+package com.MMT.helper;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.MMT.bean.HotelRoom;
+/*private String hotelId;
+	private int hotelRoomNo;
+	private String hotelRoomType;
+	private double hotelRoomPrice;
+	private String hotelRoomStatus;
+ * */
+public class HotelRoomHelperMMT {
+	HotelRoom hotelRoom=new HotelRoom();
+	Scanner sc=new Scanner(System.in);
+	ArrayList<HotelRoom> arrayListHotelRoom=new ArrayList<HotelRoom>();
+public void input(){
+	System.out.println("Enter Hotel ID");
+	hotelRoom.setHotelId(sc.next());
+	System.out.println("Enter Hotel Room No: :");
+	hotelRoom.setHotelRoomNo(sc.nextInt());
+	System.out.println("Enter Hotel Room Type :");
+	hotelRoom.setHotelRoomType(sc.next());
+	System.out.println("Enter Hotel Room Price :");
+	hotelRoom.setHotelRoomPrice(sc.nextDouble());
+	System.out.println("Enter Hotel Room Status :");
+	hotelRoom.setHotelRoomStatus(sc.next());
+	arrayListHotelRoom.add(hotelRoom);
+	
+}
+public HotelRoom getHotelRoom() {
+	return hotelRoom;
+}
+public void setHotelRoom(HotelRoom hotelRoom) {
+	this.hotelRoom = hotelRoom;
+}
+public ArrayList<HotelRoom> getArrayListHotelRoom() {
+	return arrayListHotelRoom;
+}
+public void setArrayListHotelRoom(ArrayList<HotelRoom> arrayListHotelRoom) {
+	this.arrayListHotelRoom = arrayListHotelRoom;
+}
+}
