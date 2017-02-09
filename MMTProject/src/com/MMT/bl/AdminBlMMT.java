3101f18393f2090c2f70458ad55033056323fe3f
Rupank <rupankbansal@gmail.com>
2017-02-09 10:34:35 +0530

    Indentaion

--- MMTProject/src/com/MMT/bl/AdminBlMMT.java
+++ MMTProject/src/com/MMT/bl/AdminBlMMT.java
@@ -13,42 +13,42 @@ import com.MMT.dao.HotelDaoImplMMT;
 import com.MMT.dao.UserDaoImplMMT;
 
 public class AdminBlMMT {
-	private FlightDaoImplMMT F = new FlightDaoImplMMT();
-	private HotelDaoImplMMT H = new HotelDaoImplMMT();
-	private AdminDaoImplMMT A = new AdminDaoImplMMT();
+	private FlightDaoImplMMT flightdao = new FlightDaoImplMMT();
+	private HotelDaoImplMMT hoteldao = new HotelDaoImplMMT();
+	private AdminDaoImplMMT admindao = new AdminDaoImplMMT();
 
 	public int insertHotel(Hotel h) throws ClassNotFoundException, IOException, SQLException {
-		return H.insertHotel(h);
+		return hoteldao.insertHotel(h);
 	}
 
 	public int insertFlight(Flight f) throws ClassNotFoundException, SQLException {
-		return F.insertFlight(f);
+		return flightdao.insertFlight(f);
 	}
 
 	public int deleteHotel(String hotelId) throws ClassNotFoundException, SQLException {
-		return H.deleteHotel(hotelId);
+		return hoteldao.deleteHotel(hotelId);
 	}
 
 	public int deleteFlight(String flightId) throws ClassNotFoundException, SQLException {
-		return F.deleteFlight(flightId);
+		return flightdao.deleteFlight(flightId);
 	}
 
 	public int modifyHotel(String hotelId, Hotel newhotel) throws ClassNotFoundException, SQLException {
-		return H.updateHotel(hotelId, newhotel);
+		return hoteldao.updateHotel(hotelId, newhotel);
 	}
 
 	public int modifyFlight(String flightId, Flight newflight) throws ClassNotFoundException, SQLException {
-		return F.updateFlight(flightId, newflight);
+		return flightdao.updateFlight(flightId, newflight);
 	}
 
 	public Admin searchAdmin(String adminId) throws SQLException {
-		return A.search(adminId);
+		return admindao.search(adminId);
 	}
 
 	public boolean addAdmin(Admin admin) throws SQLException {
-		if (A.search(admin.getAdminName()) == null) {
+		if (admindao.search(admin.getAdminName()) == null) {
 
-			A.insert(admin);
+			admindao.insert(admin);
 			return true;
 		} else {
 			System.out.println("UserName already taken.Please select another  Username.");
@@ -57,16 +57,16 @@ public class AdminBlMMT {
 	}
 
 	public int removeAdmin(String adminId) throws SQLException {
-		return A.delete(adminId);
+		return admindao.delete(adminId);
 	}
 
 	public int modifyAdmin(String adminId, Admin admin) throws SQLException {
-		return A.update(adminId, admin);
+		return admindao.update(adminId, admin);
 	}
 
 	public Admin checkAdminLogin(String username, String password) throws SQLException {
 
-		Admin admin = (Admin) A.search(username);
+		Admin admin = (Admin) admindao.search(username);
 		if (admin.getAdminId().equals(username) && admin.getAdminPassword().equals(password)) {
 			return admin;
 		}
--- MMTProject/src/com/MMT/bl/FlightBookingBlMMT.java
+++ MMTProject/src/com/MMT/bl/FlightBookingBlMMT.java
@@ -3,81 +3,74 @@ package com.MMT.bl;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 import com.MMT.bean.Flight;
 import com.MMT.bean.FlightBooking;
 import com.MMT.dao.FlightBookingDaoMMT;
 import com.MMT.dao.FlightBookingImpMMT;
 import com.MMT.dao.FlightDaoImplMMT;
 
-
-
 public class FlightBookingBlMMT {
-	
-	
-	
-	FlightDaoImplMMT F=new FlightDaoImplMMT();
-	FlightBookingDaoMMT FB=new FlightBookingImpMMT();
-	
-	private boolean checkAvailability(Flight F,int num){
-		boolean status=false;
-		int var=num;
-		Flight f=F;
-		if(f.getAvailableSeats()-var<0){
+
+	FlightDaoImplMMT flightdao = new FlightDaoImplMMT();
+	FlightBookingDaoMMT flightBookingDao = new FlightBookingImpMMT();
+
+	private boolean checkAvailability(Flight F, int num) {
+		boolean status = false;
+		int var = num;
+		Flight f = F;
+		if (f.getAvailableSeats() - var < 0) {
 			return status;
-		}
-		else{
-			status=true;
+		} else {
+			status = true;
 			return status;
 		}
 	}
-	
-	public ArrayList<Flight> displayFlight() throws ClassNotFoundException, SQLException{
-		return F.displayFlight();
-		}
 
-	public Flight searchFlight(String flightId) throws ClassNotFoundException, SQLException{
-		return F.searchFlight(flightId);
-		}
-	public ArrayList<Flight> searchFlight(String flightSource, String flightDestination) throws ClassNotFoundException, SQLException{
-		return F.searchFlight(flightSource, flightDestination);
-		}
-	
-	
-	public FlightBooking bookFlight(String userId,String flightId,String flightSource, String flightDestination, int seats) throws ClassNotFoundException, SQLException{
-		String fid=flightId;
-		String UId=userId;
-		boolean flag=false;
-		int rseats=seats;
-		Flight f=F.searchFlight(fid);
-		if(f==null){
+	public ArrayList<Flight> displayFlight() throws ClassNotFoundException, SQLException {
+		return flightdao.displayFlight();
+	}
+
+	public Flight searchFlight(String flightId) throws ClassNotFoundException, SQLException {
+		return flightdao.searchFlight(flightId);
+	}
+
+	public ArrayList<Flight> searchFlight(String flightSource, String flightDestination)
+			throws ClassNotFoundException, SQLException {
+		return flightdao.searchFlight(flightSource, flightDestination);
+	}
+
+	public FlightBooking bookFlight(String userId, String flightId, String flightSource, String flightDestination,
+			int seats) throws ClassNotFoundException, SQLException {
+		String fid = flightId;
+		String UId = userId;
+		boolean flag = false;
+		int rseats = seats;
+		Flight f = flightdao.searchFlight(fid);
+		if (f == null) {
 			return null;
-		}
-		else{
-			if(checkAvailability(f, rseats)!=true){
+		} else {
+			if (checkAvailability(f, rseats) != true) {
 				System.out.println("Seats Not Available!!");
 				return null;
-			}
-			else{
-				f.setAvailableSeats(f.getAvailableSeats()-rseats);
+			} else {
+				f.setAvailableSeats(f.getAvailableSeats() - rseats);
 				System.out.println("Seats Available!!");
-				//Date d=new Date();
-				FlightBooking FBD=new FlightBooking();
-				        
-				int hbid = 10000 + (int)(Math.random() * 11000); 
-				String id=Integer.toString(hbid);
-				String date=new String();
+				// Date d=new Date();
+				FlightBooking FBD = new FlightBooking();
+
+				int hbid = 10000 + (int) (Math.random() * 11000);
+				String id = Integer.toString(hbid);
+				String date = new String();
 				FBD.setUserId(UId);
 				FBD.setFlightId(fid);
 				FBD.setFlightBookingId(id);
 				FBD.setFlightBookingId(date);
 				FBD.setFlag(true);
-				FB.insertFlightBooking(FBD);
-				F.updateFlight(fid, f);
+				flightBookingDao.insertFlightBooking(FBD);
+				flightdao.updateFlight(fid, f);
 				return FBD;
 			}
 		}
 	}
-	
-	
+
 }
