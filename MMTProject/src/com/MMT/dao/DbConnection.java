5a6374b24390f2e17193531b1120d226e5f23416
Rahul <rku189@DELBAHAMAS46635.sapient.com>
2017-02-09 11:50:09 +0530

    defgnahC

--- MMTProject/src/com/MMT/dao/DbConnection.java
+++ MMTProject/src/com/MMT/dao/DbConnection.java
@@ -9,35 +9,35 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DbConnection {
-	public static Connection dbConnection() throws IOException, ClassNotFoundException, SQLException{
-FileInputStream fis=new FileInputStream("MyproFile.properties");
-		
-		Properties p = new Properties();
-		
-		p.load(fis);
-		
-		String driver = p.getProperty("dn");
-		String url = p.getProperty("url");
-		String user = p.getProperty("user");
-		String pwd = p.getProperty("pwd");
-		
-		Connection con=null;
-		Class.forName(driver);
-		//Connect to databases
-		con=DriverManager.getConnection(url, user, pwd);
-		
-		return con;
-	
-//			try {
-//				Class.forName("oracle.jdbc.driver.OracleDriver");
-//				con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521",
-//						"system", "sapient@123");
-//			} catch (ClassNotFoundException | SQLException e) {
-//			
-//				e.printStackTrace();
-//				System.out.println("this==============");
-//			}
-			
+	public static Connection dbConnection() throws IOException, ClassNotFoundException, SQLException {
+//		FileInputStream fis = new FileInputStream("MyproFile.properties");
+//
+//		Properties p = new Properties();
+//
+//		p.load(fis);
+//
+//		String driver = p.getProperty("dn");
+//		String url = p.getProperty("url");
+//		String user = p.getProperty("user");
+//		String pwd = p.getProperty("pwd");
+
+		Connection con = null;
+	//	Class.forName(driver);
+		// Connect to databases
+		//con = DriverManager.getConnection(url, user, pwd);
+
 		//return con;
+
+		 try {
+		 Class.forName("oracle.jdbc.driver.OracleDriver");
+		 con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521",
+		 "system", "sapient@123");
+		 } catch (ClassNotFoundException | SQLException e) {
+		
+		 e.printStackTrace();
+		 System.out.println("this==============");
+		 }
+
+		 return con;
 	}
 }
