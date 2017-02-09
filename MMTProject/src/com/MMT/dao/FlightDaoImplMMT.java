fb6a0a77ce99915f720e727118d6e61cd50dec14
shreyoshi <shreyoshimahato@gmail.com>
2017-02-09 12:51:58 +0530

    lpmidf

--- MMTProject/src/com/MMT/dao/FlightDaoImplMMT.java
+++ MMTProject/src/com/MMT/dao/FlightDaoImplMMT.java
@@ -28,7 +28,9 @@ public class FlightDaoImplMMT implements FlightDaoMMT {
 		//Testing//server
 		//Query
 		Statement stmt=con.createStatement();
-		int rows=stmt.executeUpdate("insert into Flight( flightCompanyName,flightId,flightSource,flightDestination,flightDepartureTime,flightArrivalTime,flightTicketPrice ,availableSeats)  values ("+flightCompanyName+","+flightId+","+flightSource+","+flightDestination+","+flightDepartureTime+","+flightArrivalTime+","+flightTicketPrice+","+availableSeats+")");
+		//int rows=stmt.executeUpdate("insert into InfoSignee values("+id+","+ct+","+pwd+","+ename+","+parkNo+")");
+		int rows=stmt.executeUpdate("insert into Flight values ("+flightCompanyName+","+flightId+","+flightSource+","+flightDestination+","+flightDepartureTime+","+flightArrivalTime+","+flightTicketPrice+","+availableSeats+")");
+		//int rows=stmt.executeUpdate("insert into Flight( flightCompanyName,flightId,flightSource,flightDestination,flightDepartureTime,flightArrivalTime,flightTicketPrice ,availableSeats)  values ("+flightCompanyName+","+flightId+","+flightSource+","+flightDestination+","+flightDepartureTime+","+flightArrivalTime+","+flightTicketPrice+","+availableSeats+")");
 		System.out.println("Inserted");
 		//Process Results
 		
